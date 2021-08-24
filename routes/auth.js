a97f2a374947d0300d2efdf585b0207fc8ce7cbc
Luiz <ruivolfb@gmail.com>
2021-08-24 18:07:27 -0300

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, resp) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
 
     })
+
+    try {
+        const savedUser = await user.save();
+        resp.send(savedUser);
+    } catch (error) {
+        resp.status(400).send(error);
+    }
 })
 
 router.post('/login')
 
 module.exports = router;
- 
